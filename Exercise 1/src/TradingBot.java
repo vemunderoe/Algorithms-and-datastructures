bbdfb7c264c9aeac2efe8234e207b7b540b868ed
Vemund E. Røe <vemunderoe@gmail.com>
2023-08-24 13:22:38 +0200

    Updated TradingBot with time-table

--- Exercise 1/src/TradingBot.java
+++ Exercise 1/src/TradingBot.java
@@ -5,41 +5,73 @@ public class TradingBot {
   public static Random random = new Random();
   public static void main(String[] args) {
     int[] priceChanges = {-1, 3, -9, 2, 2, -1, 2, -1, -5};
-    int[] priceChanges2 = {-1, -1, -1, -1, -1, -1};
-    int days = 100000;
-    int[] priceChanges3 = randomPriceChanges(days);
 
-    Date startTime = new Date();
-    int rounds = 0;
+    // Test ON with priceChanges
     int[] bestBuySellDay; // {profit, buyDay, sellDay}
-    Date endTime;
-
     bestBuySellDay = ON(priceChanges);
+    System.out.println("### Test of O(n) with priceChanges from text-book ###");
     System.out.printf("Best day to buy is day %d and best day to sell is day %d\n", bestBuySellDay[1], bestBuySellDay[2]);
 
-    // For O(n)
+    System.out.println();
+    System.out.println("### Time Complexity O(n) ###");
+    System.out.println("n-days - milliseconds");
+    testONWithNDays(100000);
+    testONWithNDays(1000000);
+    testONWithNDays(10000000);
+
+    System.out.println();
+    System.out.println("### Time Complexity O(n^2) ###");
+    System.out.println("n-days - milliseconds");
+    testON2WithNDays(1000);
+    testON2WithNDays(10000);
+    testON2WithNDays(100000);
+  }
+
+  /**
+   * Method to test the time complexity of O(n) with n-days
+   * @param nDays n-days
+   */
+  public static void testONWithNDays(int nDays) {
+    int[] bestBuySellDay; // {profit, buyDay, sellDay}
+    int[] priceChangesNDays = randomPriceChanges(nDays);
+    Date startTime = new Date();
+    Date endTime;
+    int rounds = 0;
     do {
-      bestBuySellDay = ON(priceChanges3);
+      bestBuySellDay = ON(priceChangesNDays);
       endTime = new Date();
       rounds++;
     } while (endTime.getTime() - startTime.getTime() < 1000);
 
     double time = (double) (endTime.getTime() - startTime.getTime()) / rounds;
-    System.out.println("Milliseconds per round: " + time);
-    System.out.printf("Best day to buy is day %d and best day to sell is day %d\n", bestBuySellDay[1], bestBuySellDay[2]);
+    System.out.printf("%d - %.2f\n", nDays, time);
+  }
 
-    // For O(n^2)
+  /**
+   * Method to test the time complexity of O(n^2) with n-days
+   * @param nDays n-days
+   */
+  public static void testON2WithNDays(int nDays) {
+    int[] bestBuySellDay; // {profit, buyDay, sellDay}
+    int[] priceChangesNDays = randomPriceChanges(nDays);
+    Date startTime = new Date();
+    Date endTime;
+    int rounds = 0;
     do {
-      bestBuySellDay = ON2(priceChanges3);
+      bestBuySellDay = ON2(priceChangesNDays);
       endTime = new Date();
       rounds++;
     } while (endTime.getTime() - startTime.getTime() < 1000);
 
-    time = (double) (endTime.getTime() - startTime.getTime()) / rounds;
-    System.out.println("Milliseconds per round: " + time);
-    System.out.printf("Best day to buy is day %d and best day to sell is day %d\n", bestBuySellDay[1], bestBuySellDay[2]);
+    double time = (double) (endTime.getTime() - startTime.getTime()) / rounds;
+    System.out.printf("%d - %.2f\n", nDays, time);
   }
 
+  /**
+   * Method to generate an array of random price changes with n-days
+   * @param length n-days
+   * @return array of random price changes
+   */
   public static int[] randomPriceChanges(int length) {
     int[] priceChanges = new int[length];
     for (int i = 0; i < length; i++) {
@@ -48,6 +80,11 @@ public class TradingBot {
     return priceChanges;
   }
 
+  /**
+   * Method to find the best day to buy and sell stock with O(n^2) time complexity
+   * @param priceChanges array of price changes
+   * @return array of best buy-day and sell-day
+   */
   public static int[] ON2(int[] priceChanges) {
     int[] bestBuySellDay = {0, 0, 0}; // {profit, buyDay, sellDay}
 
@@ -66,6 +103,11 @@ public class TradingBot {
     return bestBuySellDay;
   }
 
+  /**
+   * Method to find the best day to buy and sell stock with O(n) time complexity
+   * @param priceChanges array of price changes
+   * @return array of best buy-day and sell-day
+   */
   public static int[] ON(int[] priceChanges) {
     // create array of stock value from the price changes
     int[] stockValues = new int[priceChanges.length + 1];
