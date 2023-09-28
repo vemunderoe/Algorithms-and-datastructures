9efdcc2509792ac65bc76046caf4ae6d19e71f66
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2023-09-28 14:39:48 +0200

    Update Hasher2.java

--- Exercise 5/Hasher2.java
+++ Exercise 5/Hasher2.java
@@ -7,15 +7,28 @@ import java.util.stream.LongStream;
 public class Hasher2 {
     static Random random = new Random();
     public static void main(String[] args) {
-        int tableSize = 10007421;
+        int tableSize = 10000019;
         int numberOfValues = 10000000;
         double[] fillRatios = new double[]{0.5, 0.8, 0.9, 0.99, 1};
         long[] values = generateRandomUniqueArray(numberOfValues);
 
+        long testSearchValue = values[random.nextInt(tableSize - 1)];
+
         for (double fillRatio : fillRatios) {
             HashTable linearProbingHashTable = new LinearProbingHashTable(tableSize);
             testHashTable(linearProbingHashTable, values, fillRatio);
+
+            HashTable doubleHashingHashTable = new DoubleHashingHashTable(tableSize);
+            testHashTable(doubleHashingHashTable, values, fillRatio);
+
+            int indexLinearProbingHash = linearProbingHashTable.search(testSearchValue);
+            System.out.println("Index of test value (" + testSearchValue +  ") is: " + indexLinearProbingHash + " in linearProbingHashTable");
+
+            int indexDoubleHashingHash = doubleHashingHashTable.search(testSearchValue);
+            System.out.println("Index of test value (" + testSearchValue +  ") is: " + indexDoubleHashingHash + " in doubleHashingHashTable");
+            System.out.println("------------------------------");
         }
+
     }
 
     public static void testHashTable(HashTable hashTable, long[] values, double fillRatio) {
@@ -50,7 +63,7 @@ public class Hasher2 {
 abstract class HashTable {
     long[] table;
     int tableSize;
-    int collisions = 0;
+    long collisions = 0;
 
     public HashTable(int tableSize) {
         table = new long[tableSize];
@@ -58,14 +71,14 @@ abstract class HashTable {
     }
 
     public abstract void insert(long value);
-    public abstract void search(long value);
+    public abstract int search(long value);
 
     public int primaryHash(long value) {
-        return (int) value % tableSize;
+        return (int) (value % tableSize);
     }
 
     public int secondaryHash(long value) {
-        return (int) value % (tableSize - 1) + 1;
+        return (int) (value % (tableSize - 1) + 1);
     }
 
     public long[] getTable() {
@@ -81,18 +94,29 @@ class LinearProbingHashTable extends HashTable {
 
     @Override
     public void insert(long value) {
-        int i = 1;
         int hash = primaryHash(value);
+        int initialHash = hash;
         while (table[hash] != 0) {
             collisions++;
-            hash += 1;
+            hash = primaryHash(hash + 1);
+            if (hash == initialHash) {
+                return;
+            }
         }
         table[hash] = value;
     }
 
     @Override
-    public void search(long value) {
-
+    public int search(long value) {
+        int hash = primaryHash(value);
+        int initialHash = hash;
+        while (table[hash] != value) {
+            hash = primaryHash(hash + 1);
+            if (hash == initialHash) {
+                return -1;
+            }
+        }
+        return hash;
     }
 }
 
@@ -111,7 +135,8 @@ class DoubleHashingHashTable extends HashTable {
         }
         int hash2 = secondaryHash(value);
         for (;;) {
-            hash += hash2;
+            collisions++;
+            hash = primaryHash(hash + hash2);
             if (table[hash] == 0) {
                 table[hash] = value;
                 return;
@@ -120,7 +145,21 @@ class DoubleHashingHashTable extends HashTable {
     }
 
     @Override
-    public void search(long value) {
-
+    public int search(long value) {
+        int hash = primaryHash(value);
+        int initialHash = hash;
+        if (table[hash] == value) {
+            return hash;
+        }
+        int hash2 = secondaryHash(value);
+        for (;;) {
+            hash = primaryHash(hash + hash2);
+            if (hash == initialHash) {
+                return -1;
+            }
+            if (table[hash] == value) {
+                return hash;
+            }
+        }
     }
 }
